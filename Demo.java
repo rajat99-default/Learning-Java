2c09c66fa4be522b20f61fb8da889f13dad03a09
Rajat Sujane <rajatsujane6@gmail.com>
2024-01-10 18:37:45 +0530

    first commit

---
+++ Demo.java
@@ -0,0 +1,9 @@
+// Demo.java
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+    }
+}
